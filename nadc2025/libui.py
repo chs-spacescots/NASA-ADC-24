2785262b89066d76ab01b4ec3f101fb337e50bbd
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2024-11-02 21:46:03 -0700

    fixed the big ui bug (im dum)

--- nadc2025/libui.py
+++ nadc2025/libui.py
@@ -29,48 +29,27 @@ def add_elements(uielements, autoscale_all=True, parent=None):
 print(f"Ursina Window Size: {ursina.window.size}")
 #assuming a windowed screen size of Vec2(1280, 720)...
 
-#caveman checks
-# if platform.system() == "Darwin":  # macOS
-#     button_scale = 0.005
-#     button_spacing = 0.006
-#     text_size = 0.09
-#     y_position = -.02
-#     x_position = -.02
-#     text_scale = (text_size, .1)
-# elif platform.system() == "Windows": #Windows
-#     button_scale = 0.1
-#     button_spacing = 0.06
-#     text_size = 0.9
-#     y_position = -0.3
-#     x_position = -0.2
-#     text_scale = (text_size, 1)
-# else: #Linux, pls change vals as you see fit
-#     button_scale = 0.1
-#     button_spacing = 0.06
-#     text_size = 0.9
-#     y_position = -.075
-#     x_position = -.02
-#     text_scale = (text_size, 1)
-
 #...convert to caveman SCALING factors
-size_scale=pos_scale=text_scale = 1
-if platform.system() == "Darwin":  # macOS
-    size_scale = .05#=.05 (but why?)
-    x_scale  = .05
-    y_scale  = .05
-    text_scale = .1
-elif platform.system() == "Windows": #Windows
-    # please adjust these values until TL,TR,BL,BR are at the corners of your screen!!
-    size_scale = 1
-    x_scale  = 1.775
-    y_scale  = 1
-    text_scale = 1
-else: #Linux
-    # please adjust these values until TL,TR,BL,BR are at the corners of your screen!!
-    size_scale = 1
-    x_scale  = .1
-    y_scale  = .1
-    text_scale = 1
+size_scale=pos_scale=text_scale=y_scale=x_scale= 1
+#the big bug is fixed by unfocusing & refocusing the window.
+# if platform.system() == "Darwin":  # macOS
+#     # size_scale = .05#=.05 (but why?)
+#     # x_scale  = .05
+#     # y_scale  = .05
+#     # text_scale = .1
+#     pass
+# elif platform.system() == "Windows": #Windows
+#     # please adjust these values until TL,TR,BL,BR are at the corners of your screen!!
+#     size_scale = 1
+#     x_scale  = 1.775
+#     y_scale  = 1
+#     text_scale = 1
+# else: #Linux
+#     # please adjust these values until TL,TR,BL,BR are at the corners of your screen!!
+#     size_scale = 1
+#     x_scale  = .1
+#     y_scale  = .1
+#     text_scale = 1
 
 def refit():
     for element in _dynamic:
